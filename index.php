b2587ca69dc0fe6fcfa0d1fd3be33da606da0dc6
xelon21 <cetecom@LC08L1217>
2017-06-15 14:02:42 -0400

    seguir trabajando en el slider

--- index.php
+++ index.php
@@ -7,11 +7,53 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+        <script src="jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="w3-content w3-display-container" style="max-width:800px">
+            <img class="mySlides" src="img/hombremoda.jpg" width="600px" height="400px">
+            <img class="mySlides" src="img/mujermoda.jpg" width="600px" height="400px">
+            <img class="mySlides" src="img/bebemoda.jpg" width="600px" height="400px">
+      <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+          <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+          <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+         <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+          <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+      </div>
+   </div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+</script>
+
+        </script>
     </body>
 </html>
